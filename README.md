8f93541e200cb2c414c2d0f2648b1f9ef7a552d7
Sublimeful <Qiangwu2000@gmail.com>
2023-10-29 21:12:56 -0500

    Added an installer

---
+++ README.md
@@ -0,0 +1,4 @@
+To install on Linux/MacOS:
+```
+bash <(curl -s https://raw.githubusercontent.com/Sublimeful/nvim/main/utils/installer/install.sh)
+```
---
+++ utils/installer/install.sh
@@ -0,0 +1,350 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+OS="$(uname -s)"
+
+#Set branch to main unless specified by the user
+declare -x GIT_BRANCH="${GIT_BRANCH:-"main"}"
+declare -xr GIT_REMOTE="${GIT_REMOTE:-Sublimeful/nvim.git}"
+declare -xr INSTALL_PREFIX="${INSTALL_PREFIX:-"$HOME/.local"}"
+
+declare -xr XDG_DATA_HOME="${XDG_DATA_HOME:-"$HOME/.local/share"}"
+declare -xr XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"$HOME/.config"}"
+declare -xr XDG_CACHE_HOME="${XDG_CACHE_HOME:-"$HOME/.cache"}"
+
+declare -xr NVIM_APPNAME="${NVIM_APPNAME:-"nvim"}"
+
+declare -xr RUNTIME_DIR="${RUNTIME_DIR:-"$XDG_DATA_HOME/$NVIM_APPNAME"}"
+declare -xr CONFIG_DIR="${CONFIG_DIR:-"$XDG_CONFIG_HOME/$NVIM_APPNAME"}"
+declare -xr CACHE_DIR="${CACHE_DIR:-"$XDG_CACHE_HOME/$NVIM_APPNAME"}"
+declare -xr PACKER_DIR="${PACKER_DIR:-"$RUNTIME_DIR/site/pack/packer/start/packer.nvim"}"
+
+declare ARGS_INSTALL_DEPENDENCIES=1
+declare INTERACTIVE_MODE=1
+
+declare -a __nvim_dirs=(
+  "$CACHE_DIR"
+  "$CONFIG_DIR"
+  "$PACKER_DIR"
+)
+
+declare -a __npm_deps=(
+  "neovim"
+)
+
+if ! command -v tree-sitter &>/dev/null; then
+  __npm_deps+=("tree-sitter-cli")
+fi
+
+declare -a __pip_deps=(
+  "pynvim"
+)
+
+declare -a __rust_deps=(
+  "rg::ripgrep"
+)
+
+function usage() {
+  echo "Usage: install.sh [<options>]"
+  echo ""
+  echo "Options:"
+  echo "    -h, --help                               Print this help message"
+  echo "    -y, --yes                                Disable confirmation prompts (answer yes to all questions)"
+  echo "    --[no-]install-dependencies              Whether to automatically install external dependencies (will prompt by default)"
+}
+
+function parse_arguments() {
+  while [ "$#" -gt 0 ]; do
+    case "$1" in
+      -y | --yes)
+        INTERACTIVE_MODE=0
+        ;;
+      --install-dependencies)
+        ARGS_INSTALL_DEPENDENCIES=1
+        ;;
+      --no-install-dependencies)
+        ARGS_INSTALL_DEPENDENCIES=0
+        ;;
+      -h | --help)
+        usage
+        exit 0
+        ;;
+    esac
+    shift
+  done
+}
+
+function msg() {
+  local text="$1"
+  local div_width="80"
+  printf "%${div_width}s\n" ' ' | tr ' ' -
+  printf "%s\n" "$text"
+}
+
+function confirm() {
+  local question="$1"
+  while true; do
+    msg "$question"
+    read -p "[y]es or [n]o (default: no) : " -r answer
+    case "$answer" in
+      y | Y | yes | YES | Yes)
+        return 0
+        ;;
+      n | N | no | NO | No | *[[:blank:]]* | "")
+        return 1
+        ;;
+      *)
+        msg "Please answer [y]es or [n]o."
+        ;;
+    esac
+  done
+}
+
+function stringify_array() {
+  echo -n "${@}" | sed 's/ /, /'
+}
+
+function main() {
+  parse_arguments "$@"
+
+  print_logo
+
+  msg "Detecting platform for managing any additional neovim dependencies"
+
+  detect_platform
+
+  check_system_deps
+
+  if [ "$ARGS_INSTALL_DEPENDENCIES" -eq 1 ]; then
+    if [ "$INTERACTIVE_MODE" -eq 1 ]; then
+      if confirm "Would you like to install NodeJS dependencies: $(stringify_array "${__npm_deps[@]}")?"; then
+        install_nodejs_deps
+      fi
+      if confirm "Would you like to install Python dependencies: $(stringify_array "${__pip_deps[@]}")?"; then
+        install_python_deps
+      fi
+      if confirm "Would you like to install Rust dependencies: $(stringify_array "${__rust_deps[@]}")?"; then
+        install_rust_deps
+      fi
+    else
+      install_nodejs_deps
+      install_python_deps
+      install_rust_deps
+    fi
+  fi
+
+  verify_nvim_dirs
+
+  clone
+
+  setup
+
+  msg "Thank you for installing!!!"
+  echo "Do not forget to use a font with glyphs (icons) support [https://github.com/ryanoasis/nerd-fonts]"
+  echo "Make sure to run :PackerSync when starting nvim for the first time"
+}
+
+function detect_platform() {
+  case "$OS" in
+    Linux)
+      if [ -f "/etc/arch-release" ] || [ -f "/etc/artix-release" ]; then
+        RECOMMEND_INSTALL="sudo pacman -S"
+      elif [ -f "/etc/fedora-release" ] || [ -f "/etc/redhat-release" ]; then
+        RECOMMEND_INSTALL="sudo dnf install -y"
+      elif [ -f "/etc/gentoo-release" ]; then
+        RECOMMEND_INSTALL="emerge -tv"
+      else # assume debian based
+        RECOMMEND_INSTALL="sudo apt install -y"
+      fi
+      ;;
+    FreeBSD)
+      RECOMMEND_INSTALL="sudo pkg install -y"
+      ;;
+    NetBSD)
+      RECOMMEND_INSTALL="sudo pkgin install"
+      ;;
+    OpenBSD)
+      RECOMMEND_INSTALL="doas pkg_add"
+      ;;
+    Darwin)
+      RECOMMEND_INSTALL="brew install"
+      ;;
+    *)
+      echo "OS $OS is not currently supported."
+      exit 1
+      ;;
+  esac
+}
+
+function print_missing_dep_msg() {
+  if [ "$#" -eq 1 ]; then
+    echo "[ERROR]: Unable to find dependency [$1]"
+    echo "Please install it first and re-run the installer. Try: $RECOMMEND_INSTALL $1"
+  else
+    local cmds
+    cmds=$(for i in "$@"; do echo "$RECOMMEND_INSTALL $i"; done)
+    printf "[ERROR]: Unable to find dependencies [%s]" "$@"
+    printf "Please install any one of the dependencies and re-run the installer. Try: \n%s\n" "$cmds"
+  fi
+}
+
+function check_neovim_min_version() {
+  local verify_version_cmd='if !has("nvim-0.8") | cquit | else | quit | endif'
+
+  # exit with an error if min_version not found
+  if ! nvim --headless -u NONE -c "$verify_version_cmd"; then
+    echo "[ERROR]: Requires at least Neovim v0.8 or higher"
+    exit 1
+  fi
+}
+
+function check_system_deps() {
+  if ! command -v git &>/dev/null; then
+    print_missing_dep_msg "git"
+    exit 1
+  fi
+  if ! command -v nvim &>/dev/null; then
+    print_missing_dep_msg "neovim"
+    exit 1
+  fi
+  check_neovim_min_version
+}
+
+function __install_nodejs_deps_pnpm() {
+  echo "Installing node modules with pnpm.."
+  pnpm install -g "${__npm_deps[@]}"
+  echo "All NodeJS dependencies are successfully installed"
+}
+
+function __install_nodejs_deps_npm() {
+  echo "Installing node modules with npm.."
+  for dep in "${__npm_deps[@]}"; do
+    if ! npm ls -g "$dep" &>/dev/null; then
+      printf "installing %s .." "$dep"
+      npm install -g "$dep"
+    fi
+  done
+
+  echo "All NodeJS dependencies are successfully installed"
+}
+
+function __install_nodejs_deps_yarn() {
+  echo "Installing node modules with yarn.."
+  yarn global add "${__npm_deps[@]}"
+  echo "All NodeJS dependencies are successfully installed"
+}
+
+function __validate_node_installation() {
+  local pkg_manager="$1"
+  local manager_home
+
+  if ! command -v "$pkg_manager" &>/dev/null; then
+    return 1
+  fi
+
+  if [ "$pkg_manager" == "npm" ]; then
+    manager_home="$(npm config get prefix 2>/dev/null)"
+  elif [ "$pkg_manager" == "pnpm" ]; then
+    manager_home="$(pnpm config get prefix 2>/dev/null)"
+  else
+    manager_home="$(yarn global bin 2>/dev/null)"
+  fi
+
+  if [ ! -d "$manager_home" ] || [ ! -w "$manager_home" ]; then
+    return 1
+  fi
+
+  return 0
+}
+
+function install_nodejs_deps() {
+  local -a pkg_managers=("pnpm" "yarn" "npm")
+  for pkg_manager in "${pkg_managers[@]}"; do
+    if __validate_node_installation "$pkg_manager"; then
+      eval "__install_nodejs_deps_$pkg_manager"
+      return
+    fi
+  done
+  echo "[WARN]: skipping installing optional nodejs dependencies due to insufficient permissions."
+  echo "check how to solve it: https://docs.npmjs.com/resolving-eacces-permissions-errors-when-installing-packages-globally"
+}
+
+function install_python_deps() {
+  echo "Verifying that pip is available.."
+  if ! python3 -m ensurepip >/dev/null; then
+    if ! python3 -m pip --version &>/dev/null; then
+      echo "[WARN]: skipping installing optional python dependencies"
+      return 1
+    fi
+  fi
+  echo "Installing with pip.."
+  for dep in "${__pip_deps[@]}"; do
+    python3 -m pip install --user "$dep" || return 1
+  done
+  echo "All Python dependencies are successfully installed"
+}
+
+function __attempt_to_install_with_cargo() {
+  if command -v cargo &>/dev/null; then
+    echo "Installing missing Rust dependency with cargo"
+    cargo install "$1"
+  else
+    echo "[WARN]: Unable to find cargo. Make sure to install it to avoid any problems"
+    exit 1
+  fi
+}
+
+# we try to install the missing one with cargo even though it's unlikely to be found
+function install_rust_deps() {
+  for dep in "${__rust_deps[@]}"; do
+    if ! command -v "${dep%%::*}" &>/dev/null; then
+      __attempt_to_install_with_cargo "${dep##*::}"
+    fi
+  done
+  echo "All Rust dependencies are successfully installed"
+}
+
+function clone() {
+  msg "Cloning configuration"
+  if ! git clone --branch "$GIT_BRANCH" \
+    "https://github.com/${GIT_REMOTE}" "$CONFIG_DIR"; then
+    echo "Failed to clone repository. Installation failed."
+    exit 1
+  fi
+}
+
+function setup() {
+  msg "Preparing Packer setup"
+
+  git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+  printf "Packer setup complete\n"
+}
+
+function verify_nvim_dirs() {
+  for dir in "${__nvim_dirs[@]}"; do
+    if [ -d "$dir" ]; then
+      rm -rf "$dir"
+    fi
+    mkdir -p "$dir"
+  done
+  mkdir -p "$CONFIG_DIR"
+}
+
+function print_logo() {
+  cat <<'EOF'
+
+                           88\
+                           \__|
+      888888$\  88\    88\ 88\ 888888\8888\
+      88  __88\ \88\  88  |88 |88  _88  _88\
+      88 |  88 | \88\88  / 88 |88 / 88 / 88 |
+      88 |  88 |  \88$  /  88 |88 | 88 | 88 |
+      88 |  88 |   \$  /   88 |88 | 88 | 88 |
+      \__|  \__|    \_/    \__|\__| \__| \__|
+
+EOF
+}
+
+main "$@"
