004921f8fedccc09af2c3fe49a5719630cf716d5
Jian Qiang Wu <Qiangwu2000@gmail.com>
2024-09-24 23:06:53 -0500

    Corrected a comment in plugins_setup.lua

--- lua/plugins_setup.lua
+++ lua/plugins_setup.lua
@@ -41,7 +41,7 @@ require("nvim-brackets")
 
 -- nvim-treesitter
 require("nvim-treesitter.configs").setup({
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
   ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
